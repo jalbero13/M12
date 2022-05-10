2060b64341ed3243af8576e9569c3bc03f08bff7
jalbero13 <josealberosoteras@gmail.com>
2022-05-10 18:48:32 +0200

    10-05-22 funciona el seeder de cicle

--- M12ProyectoJoseSilvia/public/js/validacion.js
+++ M12ProyectoJoseSilvia/public/js/validacion.js
@@ -1,15 +1,38 @@
 let valCorreo = /^\w\@$/;
 
 
-let valTelefono = /^\[6-8-9]{1}d{8}$/;
+function validarTelefono(e){
+    let valTelefono = /^\[6-8-9]{1}d{8}$/;
+    var campo = $('#telefonoAlumno') ;
+    if(campo.value.match(valTelefono)){
+        campo.setAttribute('class', 'correcto');
+    }else{
+        campo.setAttribute('class', 'erroneo');
+        e.preventDefault();
+    }
+}
 
 
-function validarDni(){
+function validarDni(e){
+    var valDni = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/;
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-    var campo = $('dniAlumne').val()
-    var posicion = numero%23;
-    var numeros =  input.substring(0,campo.length);
-    input.length()
+    var campo = $('#dniAlumne');
+    
+    var numeros =  campo.substring(0,campo.length());
+    var posicion = numeros%23;
+    var letra = campo.substring(campo.length()-1,campo.length());
+
+    if(campo.match(valDni)){
+        if(letras[posicion].match(letra)){
+            campo.setAttribute('class', 'correcto');
+        }else{
+            campo.setAttribute('class', 'erroneo');
+            e.preventDefault();
+        }
+    }else{
+        campo.setAttribute('class', 'erroneo')
+        e.preventDefault
+    }
 }
 
 
