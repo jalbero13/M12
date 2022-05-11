a948dbe3b68a95fe68bf3f38c83597517c2f6ac9
jalbero13 <josealberosoteras@gmail.com>
2022-05-11 19:37:53 +0200

    11-05-22 añadida vista para inscribir al profesor

--- M12ProyectoJoseSilvia/resources/views/mis_vistas/addModuloUser.blade.php
+++ M12ProyectoJoseSilvia/resources/views/mis_vistas/addModuloUser.blade.php
@@ -7,10 +7,10 @@
         </x-slot>
         <ul class="nav nav-tabs" style="width: 500rem" >
             <li class="nav-item" style="font-size: 1.2rem">
-              <a class="nav-link" aria-current="page" href="/dashboard" style="background-color: #498f9d; color: lightyellow">Alumne</a>
+              <a class="nav-link" aria-current="page" href="/dashboard" style="background-color: lightyellow; color: #498f9d">Alumne</a>
             </li>
             <li class="nav-item" style="font-size: 1.2rem">
-              <a class="nav-link active" href="/profesor" style="background-color: lightyellow; color: #498f9d">Professor</a>
+              <a class="nav-link active" href="/profesor" style="background-color: #498f9d; color:lightyellow">Professor</a>
             </li>
             <li class="nav-item" style="font-size: 1.2rem">
               <a class="nav-link" href="/cicle" style="background-color: lightyellow; color: #498f9d">Cicle</a>
@@ -27,9 +27,9 @@
             <div class="row" style="margin-top: 20px">
                 <div class="col-12 col-md-6">
                     <div class="mb-3">
-                        <label class="form-label">Alumne</label>
-                        <input name="nombreAlumno" value="">
-                        <input name="idAlumno" value="{{$id}}" hidden>
+                        <label class="form-label">Professor</label>
+                        <input name="nombreProfe" value="">
+                        <input name="idProfe" value="{{$id}}" hidden>
                       </div>
                 </div>
                 <div class="col-12 col-md-6">
@@ -45,7 +45,7 @@
                 
             </div>
             <div class="d-grid gap-2">
-                <button type="submit" class="btn" style="background-color: #498f9d">Insciure alumne</button>
+                <button type="submit" class="btn" style="background-color: #498f9d">Inscriure professor</button>
             </div>
           </form>
 
