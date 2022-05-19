22fbf2aca15cbece52b01bac49239bff2c186f37
jalbero13 <josealberosoteras@gmail.com>
2022-05-19 20:36:02 +0200

    19-05-22 modificado web

--- M12ProyectoJoseSilvia/app/Http/Controllers/NotesmodulController.php
+++ M12ProyectoJoseSilvia/app/Http/Controllers/NotesmodulController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Uf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class NotesmodulController extends Controller
 {
     //
-    public function showNotesModul(){
+    public function showNotesModul($id){
         if(Auth::user()->role_id == 2){
-            return view('mis_vistas.notasModulo');
+            return view('mis_vistas.notasModulo', array('id'=>$id, 'ufs'=>Uf::where('modul_id',$id)));
         }
     }
 }
